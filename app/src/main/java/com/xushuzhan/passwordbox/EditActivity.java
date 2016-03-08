85a44f4ea13b1db74018651c96ba2842f28a1e76
XAY1 <391830728@qq.com>
2016-03-08 19:43:19 +0800

    修改了EditActivit
    添加了用户输入信息不能为空的限制。

--- app/src/main/java/com/xushuzhan/passwordbox/EditActivity.java
+++ app/src/main/java/com/xushuzhan/passwordbox/EditActivity.java
@@ -64,25 +64,35 @@ public class EditActivity extends AppCompatActivity {
         okButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                TextInputLayout ti_1 = (TextInputLayout) findViewById(R.id.p_name);
+                TextInputLayout ti_2 = (TextInputLayout) findViewById(R.id.p_account);
+                TextInputLayout ti_3 = (TextInputLayout) findViewById(R.id.p_password);
+                TextInputLayout ti_4 = (TextInputLayout) findViewById(R.id.p_note);
+                    if (edit_1.getText().toString().equals("")) {
+                        ti_1.setError("名称不能为空");
+                    }else if(edit_2.getText().toString().equals("")){
+                        ti_2.setError("用户名不能为空");
+                    } else if(edit_3.getText().toString().equals("")){
+                        ti_3.setError("密码不能为空");
+                    } else{
+                    ContentValues values = new ContentValues();
+                    //组装数据
+                    values.put("name", edit_1.getText().toString());
+                    values.put("account", edit_2.getText().toString());
+                    values.put("password", edit_3.getText().toString());
+                    values.put("note", edit_4.getText().toString());
+                    values.put("time", NowTime);
+                    //往SQLite加入数据
+                    db.insert("user", null, values);
+                    MyAdapter mAdapter = new MyAdapter();
 
-                ContentValues values=new ContentValues();
-                //组装数据
-                values.put("name",edit_1.getText().toString());
-                values.put("account",edit_2.getText().toString());
-                values.put("password",edit_3.getText().toString());
-                values.put("note",edit_4.getText().toString());
-                values.put("time",NowTime);
-                //往SQLite加入数据
-                db.insert("user",null,values);
-                MyAdapter mAdapter=new MyAdapter();
+                    Toast.makeText(EditActivity.this, "创建成功", Toast.LENGTH_SHORT).show();
+                    MainActivity.datas.removeAll(MainActivity.datas);
+                    finish();
+                    Intent intent = new Intent(EditActivity.this, MainActivity.class);
+                    startActivity(intent);
 
-               
-                Toast.makeText(EditActivity.this,"创建成功",Toast.LENGTH_SHORT).show();
-                MainActivity.datas.removeAll(MainActivity.datas);
-                finish();
-                Intent intent=new Intent(EditActivity.this,MainActivity.class);
-                startActivity(intent);
-
+                }
             }
         });
     }
